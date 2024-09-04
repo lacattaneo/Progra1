40c42ee3a9527aa13c67320c8fd709a5a6275542
lacattaneo <lacattaneo@uade.edu.ar>
2024-09-04 20:01:41 -0300

    matrices v1

---
+++ Matrices.py
@@ -0,0 +1,16 @@
+
+
+def matriz():
+    categorias=10
+    meses=12
+    matriz=[[0]*meses for i in range(categorias)]
+    return matriz
+
+def imprimir_matriz(matriz):
+    categorias = len(matriz)
+    columnas = len(matriz[0])
+    for f in range (categoria):
+        print("%3d" %matriz[f][c], end=" ")
+        
+matriz()
+print(imprimir_matriz(matriz))
